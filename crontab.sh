f9e2c1a34493b5c2c777539df3cb9df6f652d370
Bogdan Preda <bogdan.preda@themeisle.com>
2017-01-20 15:16:19 +0200

    Timespamp to crontab.sh

--- crontab.sh
+++ crontab.sh
@@ -1,2 +1,6 @@
+NOW=$(date +"%m-%d-%Y %r")
+echo "STARTED! Crontab Themeisle Wraith $NOW"
 php themeisle-wraith-service.php -m=sitemaps
 php themeisle-wraith-service.php -m=all_history_spyder
+NOW=$(date +"%m-%d-%Y %r")
+echo "FINISHED! Crontab Themeisle Wraith $NOW"
