32ff7be612431dae59e56eeb2f39ea8aaf5dcdbb
Bogdan Preda <bogdan.preda@themeisle.com>
2017-01-20 15:18:17 +0200

    Timestamp changes to crontab.sh

--- crontab.sh
+++ crontab.sh
@@ -1,6 +1,6 @@
-NOW=$(date +"%m-%d-%Y %r")
+NOW=$(date +"%d-%m-%Y %r")
 echo "STARTED! Crontab Themeisle Wraith $NOW"
 php themeisle-wraith-service.php -m=sitemaps
 php themeisle-wraith-service.php -m=all_history_spyder
-NOW=$(date +"%m-%d-%Y %r")
+NOW=$(date +"%d-%m-%Y %r")
 echo "FINISHED! Crontab Themeisle Wraith $NOW"
