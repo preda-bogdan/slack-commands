a9bbaa2091897fb45d520d90a0aaf8e74090ec1d
Bogdan Preda <bogdan.preda@themeisle.com>
2017-01-20 17:30:30 +0200

    Changed timmings

--- javascript/wait--casper.js
+++ javascript/wait--casper.js
@@ -4,5 +4,5 @@
 // ######################################################
 module.exports = function (casper, ready) {
     // make Wraith wait a bit longer before taking the screenshot
-    casper.wait(20000, ready); // you MUST call the ready() callback for Wraith to continue
+    casper.wait(10000, ready); // you MUST call the ready() callback for Wraith to continue
 }
--- javascript/wait--phantom.js
+++ javascript/wait--phantom.js
@@ -4,6 +4,6 @@
 // ######################################################
 module.exports = function (phantom, ready) {
     // make Wraith wait a bit longer before taking the screenshot
-    setTimeout(ready, 20000); // you MUST call the ready() callback for Wraith to continue
+    setTimeout(ready, 10000); // you MUST call the ready() callback for Wraith to continue
 }
 
